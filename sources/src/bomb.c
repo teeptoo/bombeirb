76ac93c42f8e9c0d635bc28daf828af7172c16ca
Julien MATHET <jmathet@enseirb-matmeca.fr>
2018-03-02 18:19:48 +0100

    Mise à jour de la structure bomb (pas d'erreur) Mise à jour de la fonction create_bombs Création de la fonction bomb_get_size_list Création de la fonction bomb_placed et bomb_display

--- sources/src/bomb.c
+++ sources/src/bomb.c
@@ -11,28 +11,30 @@
 #include <misc.h>
 #include <constant.h>
 
-struct bomb {
-	int range;
-	int x, y;
-	int map; //pas sur du type
-	int state;
-	struct bomb *next;
+
+struct bomb{
+  int range;
+  int x, y;
+  struct map* map;
+  int state;
+  struct bomb *next;
 };
 
-struct bomb *create_bombs(int x, int y, int map, int state){
+
+struct bomb *create_bombs(int x, int y, struct map* map){
 	struct bomb *bomb;
 	bomb = malloc(sizeof(*bomb));
-	// besoin de l'allocation dynamique ?
+	// besoin de l'allocation dynamique pour chaque élément?
 	bomb->x = x;
 	bomb->y = y;
 	bomb->map = map;
-	bomb->state = state;
+	bomb->state = 0;
 	bomb->next = NULL;
 
 	return bomb;
 }
 
-int sizeList(struct bomb *list)
+int bomb_get_size_list(struct bomb *list)
 {
   if (list==NULL)
   {
@@ -46,8 +48,23 @@ int sizeList(struct bomb *list)
   return size;
 }
 
-struct bomb *add_bomb(struct bomb *list,struct bomb *bomb)
+struct bomb *bomb_add(struct bomb *list,struct bomb *bomb)
 {
     bomb->next=list;
     return bomb;
+}
+
+struct bomb *bomb_placed(int x, int y, struct map* map,, struct bomb *list){
+	struct bomb *bomb;
+	bomb=create_bombs(x, y, map);
+	if (bomb_get_size_liste(list)==0){
+		return(bomb);
+	}
+	return(bomb_add(list,bomb));
+}
+
+
+void bomb_display(struct bomb* bomb) {
+	assert(bomb);
+	window_display_image(sprite_get_bomb(),bomb->x * SIZE_BLOC, bomb->y * SIZE_BLOC);
 }
