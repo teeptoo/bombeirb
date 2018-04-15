1a6dbb752aa1c1bba977bdd26cf12116a608859b
tlepine <theo.lepine@enseirb-matmeca.fr>
2018-04-15 14:30:17 +0200

    Affichage du nb de clés dans le bandeau

--- sources/src/game.c
+++ sources/src/game.c
@@ -129,7 +129,7 @@ void game_banner_display(struct game* game) {
 	for (int i = 0; i < map_get_width(map); i++)
 		window_display_image(sprite_get_banner_line(), i * SIZE_BLOC, y);
 
-	int white_bloc = ((map_get_width(map) * SIZE_BLOC) - 6 * SIZE_BLOC) / 4;
+	int white_bloc = ((map_get_width(map) * SIZE_BLOC) - 6 * SIZE_BLOC) / 7;
 	int x = white_bloc;
 	y = (map_get_height(map) * SIZE_BLOC) + LINE_HEIGHT;
 	window_display_image(sprite_get_banner_life(), x, y);
@@ -148,8 +148,13 @@ void game_banner_display(struct game* game) {
 	window_display_image(sprite_get_banner_range(), x, y);
 
 	x = 3 * white_bloc + 5 * SIZE_BLOC;
-	window_display_image(
-			sprite_get_number(player_get_range(game_get_player(game))), x, y);
+	window_display_image(sprite_get_number(player_get_range(game_get_player(game))), x, y);
+
+	x = 4 * white_bloc + 6 * SIZE_BLOC;
+	window_display_image(sprite_get_key(), x, y);
+
+	x = 4 * white_bloc + 7 * SIZE_BLOC;
+	window_display_image(sprite_get_number(player_get_nb_keys(game_get_player(game))), x, y);
 }
 
 void game_display(struct game* game) {
